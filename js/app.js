cbece2619372616fb17494c5a8fc749846ca29ed
Martin Rotelli <martin_rot@hotmail.com>
2021-11-30 20:51:29 -0300

    Portfolio MR

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//typed.js
+(function($){
+    var typed = new Typed('span.text-rotate', {
+        strings: ["Soy Desarrollador Web", "HTML, CSS, JavaSript, React, C++", "Estudio programacion"], 
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true
+    });
+})(jQuery);
+
+
+//isotope.js
+$(document).ready(function(){
+    $('.projects-wrapper').isotope({
+        filter: '*',
+        layoutMode: 'masonry'
+    });
+
+    $('.list-filter a').click(function(){
+        var selector = $(this).attr('data-filter');
+        $('.list-filter a').removeClass('active');
+        
+        $(this).addClass('active');
+        
+        $('.projects-wrapper').isotope({
+            filter: selector
+        });
+
+
+        return false;
+    });
+
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true
+        }
+    });
+
+});
